ffdce63936bff1a935fe71fb680e4ec5d52a9a1f
Hamza <hamzasiddiqui379@gmail.com>
2022-12-23 22:09:24 +0500

    Structuring files acc to MVC

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_basics/search_places_api.dart';
 import 'convert_lnglat_to_address.dart';
 import 'home_screen.dart';
-import 'pages/welcome/splash_screen.dart';
+// import 'pages/welcome/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
