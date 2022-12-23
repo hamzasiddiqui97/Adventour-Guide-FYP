32c4d2b50e1cd3c52b136885b26cdc02d44ab07b
Hamza <hamzasiddiqui379@gmail.com>
2022-12-23 22:08:17 +0500

    Structuring files acc to MVC

---
+++ lib/views/bottom_nav.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NavigationPage extends StatelessWidget {
+  NavigationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Container(
+
+    ));
+  }
+}
---
+++ lib/views/bottom_nav/my_plan.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyPlan extends StatelessWidget {
+  const MyPlan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
