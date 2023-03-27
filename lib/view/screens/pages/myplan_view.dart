440481ea8ebeda694c3435bb1c54d0ea630ae3b6
Hamza <hamzasiddiqui379@gmail.com>
2023-03-27 15:52:46 +0500

    fixing minor issues

--- lib/view/screens/pages/myplan_view.dart
+++ lib/view/screens/pages/myplan_view.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_basics/view/screens/views/my_trip_tab.dart';
 
+import '../../../core/constant/color_constants.dart';
 class MyPlan extends StatelessWidget {
   const MyPlan({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          // child: const MyTripTab()
-      )
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: ColorPalette.secondaryColor,
+          foregroundColor: ColorPalette.primaryColor,
+          title: const Text('My Trips'),
+          centerTitle: true,
+        ),
+        body: Container(
+            child: const Center(child: Text('No trips to show'),),
+        )
+      ),
     );
   }
 }
