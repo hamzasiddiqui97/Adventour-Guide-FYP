f8f0982cbf543482d7264bbc487fa21849b9282b
Hamza <hamzasiddiqui379@gmail.com>
2023-04-14 00:46:02 +0500

    Changing google maps api key

--- lib/.env.dart
+++ lib/.env.dart
@@ -1,2 +1,3 @@
 // const String googleApiKey = 'AIzaSyDBTvInL-0BgjzaCa4S92L4mFqtv4DkQ80';
-const String googleApiKey = 'AIzaSyAfll1txFzPvULc-rpH38Rw_jFeQFd1QGo';
+// const String googleApiKey = 'AIzaSyAfll1txFzPvULc-rpH38Rw_jFeQFd1QGo';
+const String googleApiKey = 'AIzaSyCpNqErHrvZU8fBAqKMx80zuxcpEiraeZ0';
